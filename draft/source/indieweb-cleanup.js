73d9cd7a6f10ed02a5ef89ae8a9b1f2304c83000
Aaron Parecki <aaron@parecki.com>
2020-07-25 16:04:31 -0700

    replace subheader and fix css

--- draft/source/indieweb-cleanup.js
+++ draft/source/indieweb-cleanup.js
@@ -4,6 +4,10 @@ function indiewebCleanup() {
 	$(".head .logo img").attr("src", "https://spec.indieweb.org/index_files/logo.svg");
 	$(".head .logo img").attr("width", "120").attr("height", "100");
 
+	// Replace subhead
+	var time = $(".head h2 time").html();
+	$(".head h2").html("IndieWeb Living Standard "+time);
+
 	// CC0
 	$(".copyright").html("<b>License:</b> Per <a href=\"https://creativecommons.org/publicdomain/zero/1.0/\">CC0</a>, to the extent possible under law, the editor(s) and contributors have waived all copyright and related or neighboring rights to this work. In addition, the editor(s) and contributors have made this specification available under the <a href=\"http://www.openwebfoundation.org/legal/the-owf-1-0-agreements/owfa-1-0\">Open Web Foundation Agreement Version 1.0</a>.");
 
@@ -18,5 +22,7 @@ function indiewebCleanup() {
 	// Remove "latest published version" link
 	$(".head dl dt:nth-of-type(2)").remove();
 	$(".head dl dd:nth-of-type(2)").remove();
+
+	$("body").css("background-image", "none");
 
 }
